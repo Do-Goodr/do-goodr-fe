5526f440172737e2e5878b2602772ed02a3b80cb
Colgan Meanor <colganmeanor7@gmail.com>
2022-02-05 13:14:37 -0600

    Set up simple useEffect hook to test console logging data from our fetch call

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Header from './Header';
 import Form from './Form';
+import apiCalls from '../utilities/apiCalls';
 import '../Styles/App.css';
 
 const App = () => {
+
+  useEffect(() => {
+    apiCalls.loadAllEvents()
+    .then(data => console.log(data))
+  }, [])
+
   return (
     <main className="App">
       <Header />
