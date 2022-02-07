333957e11d70078ec499cd48b821f6bde58be643
Michele Comfort <michelecomfort@gmail.com>
2022-02-07 06:55:12 -0800

    Add description property to CreateEvent component

--- src/Components/SignIn.tsx
+++ src/Components/SignIn.tsx
@@ -15,7 +15,7 @@ const SignIn = () => {
             <option>Food Bank</option>
         </select>
       </form>
-        <CreateEvent  date={''} category={''} start_time={''} end_time={''} volunteers_needed={''} />
+        <CreateEvent  date={''} category={''} start_time={''} end_time={''} volunteers_needed={''} description={''}/>
     </div>
   )
 }
