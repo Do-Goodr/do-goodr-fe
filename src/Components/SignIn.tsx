fd6ab1010bd968fc5492f4d8a9b0d71a70e13ea1
Michele Comfort <michelecomfort@gmail.com>
2022-02-06 22:11:53 -0800

    Return CreateEvent component and pass in props

--- src/Components/SignIn.tsx
+++ src/Components/SignIn.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import '../Styles/SignIn.css';
+import CreateEvent from './CreateEvent';
 
 const SignIn = () => {
   return (
-    <form className="SignIn">
-      <select
-        name="organization"
-        placeholder="Choose Organization">
-          <option hidden>Choose Organization</option>
-          <option>American Red Cross</option>
-          <option>Farts McGee</option>
-          <option>Food Bank</option>
-      </select>
-    </form>
+    <div>
+      <form className="SignIn">
+        <select
+          name="organization"
+          placeholder="Choose Organization">
+            <option hidden>Choose Organization</option>
+            <option>American Red Cross</option>
+            <option>Farts McGee</option>
+            <option>Food Bank</option>
+        </select>
+      </form>
+        <CreateEvent  date={''} category={''} start_time={''} end_time={''} volunteers_needed={''} />
+    </div>
   )
 }
 
