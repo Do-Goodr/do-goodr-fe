8da96bf46a7cf7689f6d48f66f3b669e18eeb17a
Michele Comfort <michelecomfort@gmail.com>
2022-02-08 21:01:37 -0800

    Add initial styles to Header component

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 import SignIn from './SignIn'
 import '../Styles/Header.css';
-import CreateEventForm from './CreateEventForm';
 import { Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <header className="Header">
-      <h1>Do Goodr</h1>
-      <h2>Do more. Do good.</h2>
-      <Link to='/newevent'><button >Add Volunteer Opp</button></Link>
-      <nav>
-        <ul>
-          <li>home</li>
-          <li>events</li>
-        </ul>
+    <header className="header">
+      <nav className='.nav-bar'>
+        
+          <Link to='/' className='nav-link'>home</Link>
+          {/* <Link>events</Link> */}
+        
       </nav>
+      <div className='title-slogan'>
+        <h1 className='title'>Do Goodr</h1>
+        <h2 className='slogan'>Do more. Do good.</h2>
+      </div>
+      <Link to='/newevent' className='add-opp-btn'>Add Volunteer Opp</Link>
       
     </header>
   );
