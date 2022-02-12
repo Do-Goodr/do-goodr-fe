30749795a203d87c619e750415fa9027c4a411e7
Colgan Meanor <colganmeanor7@gmail.com>
2022-02-12 09:55:39 -0600

    Narrow down pieces of global state held in Context, remove searchedEvents and filteredEvents from global state

--- src/Context/EventContext.js
+++ src/Context/EventContext.js
@@ -5,19 +5,25 @@ const EventContext = createContext()
 
 const EventProvider = props => {
   const [events, setEvents] = useState([])
-  const [searchedEvents, setSearchedEvents] = useState([])
-  const [filteredEvents, setFilteredEvents] = useState([])
+  // const [searchedEvents, setSearchedEvents] = useState([])
+  // const [filteredEvents, setFilteredEvents] = useState([])
   const [category, setCategory] = useState('')
   const [org, setOrg] = useState('')
 
-  const filterByCategory = (selectedCategory) => {
-    const filteredEventsByCategory = events.filter(event => event.category === selectedCategory)
-    setFilteredEvents(filteredEventsByCategory)
-    setCategory(selectedCategory)
-  }
+  // const filterByCategory = (selectedCategory) => {
+  //   const filteredEventsByCategory = events.filter(event => event.category === selectedCategory)
+  //   setFilteredEvents(filteredEventsByCategory)
+  //   setCategory(selectedCategory)
+  // }
+
+  // return (
+  //   <EventContext.Provider value={{ events, searchedEvents, filteredEvents, category, org, setFilteredEvents, setCategory, setEvents, setOrg, setSearchedEvents }}>
+  //     {props.children}  
+  //   </EventContext.Provider>
+  // )
 
   return (
-    <EventContext.Provider value={{ events, searchedEvents, filteredEvents, category, org, filterByCategory, setEvents, setOrg, setSearchedEvents }}>
+    <EventContext.Provider value={{ events,setEvents,category, setCategory, org, setOrg }}>
       {props.children}  
     </EventContext.Provider>
   )
