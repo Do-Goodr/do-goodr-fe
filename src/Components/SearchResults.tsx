76cf4ddf196c4bf412e0e4525778d51e483013c4
Colgan Meanor <colganmeanor7@gmail.com>
2022-02-12 09:54:31 -0600

    Move logic for determining events and category filtering to SearchResults component

--- src/Components/SearchResults.tsx
+++ src/Components/SearchResults.tsx
@@ -10,22 +10,48 @@ import { serialize } from 'v8';
 
 const SearchResults = () => {
 
-  const { events, searchedEvents, category, org, filteredEvents, filterByCategory, setEvents, setSearchedEvents } = useContext(EventContext)
+  const { setCategory, category, org } = useContext(EventContext)
+  const [events, setEvents] = useState([])
+  const [filteredEvents, setFilteredEvents] = useState([])
   const zip = useParams().zipcode
   const miles = useParams().mileage
+
   console.log(zip)
   console.log(miles)
-  console.log('searchresults', searchedEvents)
+  
 
   useEffect(() => {
     apiCalls.loadEventsByZipCode(zip, miles)
-      .then(data => setSearchedEvents(data.data))
+      .then(data => setEvents(data.data))
   }, [])
+
+  const filterByCategory = (selectedCategory: any) => {
+    const filteredEventsByCategory = events.filter((event: { category: any; }) => event.category === selectedCategory)
+    setCategory(selectedCategory)
+    setFilteredEvents(filteredEventsByCategory)
+  }
 
   return (
     <div>
       <h1>Search Results</h1>
-      <EventContainer events={searchedEvents}/>
+      <h3 data-cy='filter-title'>Filter by Category</h3>
+      <select
+        name="category"
+        data-cy='choose-category'
+        placeholder="Choose Category"
+        onChange={e => filterByCategory(e.target.value)}>
+        <option hidden>Choose Category</option>
+        <option value='animal-care'>Animal Care</option>
+        <option value='campaigning'>Campaigning</option>
+        <option value='community-development'>Community Development</option>
+        <option value='food-service'>Food Service</option>
+        <option value='grounds-cleanup'>Grounds Cleanup</option>
+        <option value='healthcare'>Healthcare</option>
+        <option value='nursing-home'>Nursing Home</option>
+        <option value='youth-mentorship'>Youth Mentorship</option>
+        <option value=''>Other</option>
+      </select>
+      <EventContainer events={filteredEvents} />
     </div>
   )
 
