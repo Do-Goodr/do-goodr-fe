bbce1b719c288b2c31c470c86dbaec7a7c6aae22
Colgan Meanor <colganmeanor7@gmail.com>
2022-02-12 12:16:34 -0600

    Remove commented out old code from Event Context

--- src/Context/EventContext.js
+++ src/Context/EventContext.js
@@ -5,22 +5,9 @@ const EventContext = createContext()
 
 const EventProvider = props => {
   const [events, setEvents] = useState([])
-  // const [searchedEvents, setSearchedEvents] = useState([])
-  // const [filteredEvents, setFilteredEvents] = useState([])
   const [category, setCategory] = useState('Any')
   const [org, setOrg] = useState('')
 
-  // const filterByCategory = (selectedCategory) => {
-  //   const filteredEventsByCategory = events.filter(event => event.category === selectedCategory)
-  //   setFilteredEvents(filteredEventsByCategory)
-  //   setCategory(selectedCategory)
-  // }
-
-  // return (
-  //   <EventContext.Provider value={{ events, searchedEvents, filteredEvents, category, org, setFilteredEvents, setCategory, setEvents, setOrg, setSearchedEvents }}>
-  //     {props.children}  
-  //   </EventContext.Provider>
-  // )
 
   return (
     <EventContext.Provider value={{ events,setEvents,category, setCategory, org, setOrg }}>
