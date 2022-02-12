46f8dc423fbf454f7907d0d04c002ba3b1afedf3
Colgan Meanor <colganmeanor7@gmail.com>
2022-02-12 09:58:01 -0600

    Add fetch call function to load events based on Org ID

--- src/utilities/apiCalls.js
+++ src/utilities/apiCalls.js
@@ -24,6 +24,19 @@ const apiCalls = {
             .catch(err => console.log(err))
     },
 
+    loadEventsByOrg: (orgID) => {
+        return fetch(`https://do-goodr-be.herokuapp.com/api/v1/organizations?id=${orgID}/events`)
+        .then(res => {
+
+            if (res.ok) {
+                return res.json()
+            } else {
+                throw new Error()
+            }
+        })
+        .catch(err => console.log(err))
+    },
+
     postEvent: (event) => {
         console.log('test', event)
         return fetch(`https://do-goodr-be.herokuapp.com/api/v1/events?category=${event.category}&organization_id=${event.organization_id}&name=${event.name}&address=${event.address}&start_time=${event.date}%20${event.start_time}&end_time=${event.date}%20${event.end_time}&vols_required=${event.vols_required}&description=${event.description}`, {
