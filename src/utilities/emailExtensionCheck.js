c4a5fd654d353403b31c19bf6d5758c50c5d2698
Michele Comfort <michelecomfort@gmail.com>
2022-02-14 13:20:35 -0800

    Add double OR brackets

--- src/utilities/emailExtensionCheck.js
+++ src/utilities/emailExtensionCheck.js
@@ -3,7 +3,7 @@ import React from 'react'
 const emailExtensionCheck = (email) => {
 
     if (email) {
-        if (email.includes('@') && email.includes('.com') | email.includes('.org') | email.includes('.gov') | email.includes('.edu') | email.includes('.net') ) {
+        if (email.includes('@') && email.includes('.com') || email.includes('.org') || email.includes('.gov') || email.includes('.edu') || email.includes('.net') ) {
         return true
         } else {
             return false
