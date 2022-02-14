67b9bbd7ed38f73d1d867bc813b2cf65bb477514
Adam Burgess <aburg1515@gmail.com>
2022-02-14 14:58:01 -0700

    Remove console logs

--- src/Components/EventContainer.tsx
+++ src/Components/EventContainer.tsx
@@ -10,8 +10,6 @@ import { Event } from '../utilities/Types';
 
 const EventContainer: React.FC<{ events: any[], category: string, org: number }> = ({ events, category, org }) => {
 
-  console.log(events)
-  console.log(category)
   const eventCards = events && events.map((event: any, index: any) => {
     return (
       <Opp
