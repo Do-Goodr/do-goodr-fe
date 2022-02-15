a5e3ba89011d83079d664ed6d01d5d52d6a687bf
Colgan Meanor <colganmeanor7@gmail.com>
2022-02-14 21:04:17 -0600

    Add basic styling to Opp Cards and relevant containers

--- src/Components/Opp.tsx
+++ src/Components/Opp.tsx
@@ -27,15 +27,21 @@ const Opp = ({ event }: OppProps ) => {
 
   return (
     <div className="opportunity" data-cy='opportunity'>
-      <h2 className='event-name' data-cy='event-name'>{event.name}</h2>
-      <p data-cy='event-phone'>Phone: 555-555-5555</p>
-      <p data-cy='event-category'>Category: {event.category}</p>
-      <p data-cy='event-address'>Event Address: {event.address}</p>
-      <p data-cy='event-date'>Date: {event.date}</p>
-      <p data-cy='event-time'>Time: {event.start_time} - {event.end_time}</p>
-      <p data-cy='event-volunteers'>Volunteers Needed: {event.vols_required}</p>
-      <p data-cy='event-description'>Description: {event.description}</p>
+      <div className='opp-info'>
+        <div className='opp-left'>
+          <h2 className='event-name' data-cy='event-name'>{event.name}</h2>
+          <p data-cy='event-phone'>Phone: 555-555-5555</p>
+          <p data-cy='event-address'>Event Address: {event.address}</p>
+        </div>
+        <div className='opp-right'>
+          <p data-cy='event-category'>Category: {event.category}</p>
+          <p data-cy='event-date'>Date: {event.date}</p>
+          <p data-cy='event-time'>Time: {event.start_time} - {event.end_time}</p>
+          <p data-cy='event-volunteers'>Volunteers Needed: {event.vols_required}</p>
+          <p data-cy='event-description'>Description: {event.description}</p>
+        </div>
       <DeleteButton />
+      </div>
     </div>
   );
 }
