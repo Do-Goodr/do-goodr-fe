366f38212e164c4a2405d815efe8ccf0315ae105
Adam Burgess <aburg1515@gmail.com>
2022-02-15 16:54:10 -0700

    Refactor org-sign-in cypress tests

--- cypress/integration/org-sign-in-page.js
+++ cypress/integration/org-sign-in-page.js
@@ -8,6 +8,14 @@ describe('org sign in page',() => {
         cy.visit('http://localhost:5000');
         cy.get('[href="/signin"]').click()
       })
+      cy.fixture('./org-data.json').then((allOrgs) => {
+        cy.intercept('GET', 'https://do-goodr-be.herokuapp.com/api/v1/organizations', {
+          statusCode: 200,
+          body: allOrgs
+        })
+        cy.visit('http://localhost:5000');
+        cy.get('[href="/signin"]').click()
+      })
     })
 
     it('should contain a header', () => {
@@ -15,12 +23,11 @@ describe('org sign in page',() => {
         .contains('Do Goodr');
     })
 
-    it('should allow a user to choose an organization', () => {
-        cy.get('.App > :nth-child(1) > :nth-child(1)')
+    it.only('should allow a user to choose an organization', () => {
+        cy.get('[data-cy="choose-organization"]')
             .should('exist')
-        cy.get('.SignIn > select')
-            .should('exist')
-            .should('have.length', 1)
+        cy.get('[data-cy="choose-organization"]')
+            .select(2)
     })
 
     it('should allow a user to create an organization', () => {
