dfcb086a40b3295a4e3484966ee4580fd2dde9ff
Colgan Meanor <colganmeanor7@gmail.com>
2022-02-14 21:51:54 -0600

    Add basic styling to create event form component

--- src/Components/CreateEventForm.tsx
+++ src/Components/CreateEventForm.tsx
@@ -17,7 +17,7 @@ const [description, setDescription] = useState<CreatedEvent['description']>()
 const submitEvent = (e:React.MouseEvent) => {
   e.preventDefault()
   const newEvent = {
-    organization_id: 4,
+    organization_id: 67,
     name: 'American Red Cross',
     address: address,
     phone: '(334)888-8888',
@@ -32,33 +32,33 @@ const submitEvent = (e:React.MouseEvent) => {
 }
 
 const SubmitButton = () => {
-  if (date && startTime && endTime && category && volunteers && description) {
-    return <button className="submit-btn" data-cy='enabled-submit-button' onClick={(e) => submitEvent(e)}>Submit Opportunity</button>
+  if (date && startTime && endTime && category && address && volunteers && description) {
+    return <button className="event-submit-btn-enabled" data-cy='enabled-submit-button' onClick={(e) => submitEvent(e)}>Submit Opportunity</button>
   } else {
-    return <button className="submit-btn" disabled={true} data-cy='disabled-submit-button' onClick={(e) => submitEvent(e)}>Submit Opportunity</button>
+    return <button className="event-submit-btn-disabled" disabled={true} data-cy='disabled-submit-button' onClick={(e) => submitEvent(e)}>Submit Opportunity</button>
   }
 }
 
   return (
     <div className="event-form-container">
       <form className='create-event' data-cy='create-event-form'>
-        <h2>Create New Volunteering Event</h2>
+        <h2 className='create-event-title'>Create New Volunteering Event</h2>
         <label>Date:
-          <input type='date' data-cy='set-date' onChange={(e) => setDate(e.target.value)}/>
+          <input type='date' className='date-input' data-cy='set-date' onChange={(e) => setDate(e.target.value)}/>
         </label>
         <div className='time-inputs'>
         <label>Start Time:
-          <input type='time' data-cy='set-start-time' onChange={(e) => setStartTime(e.target.value)}/>
+          <input type='time' className='start-time-input' data-cy='set-start-time' onChange={(e) => setStartTime(e.target.value)}/>
         </label>
         <label>End Time:
-          <input type='time' data-cy='set-end-time' onChange={(e) => setEndTime(e.target.value)}/>
+          <input type='time' className='end-time-input' data-cy='set-end-time' onChange={(e) => setEndTime(e.target.value)}/>
         </label>
         </div>
         <label>Address:
-          <input type='text' data-cy='set-address' onChange={(e) => setAddress(e.target.value)}/>
+          <input type='text' className='address-input' data-cy='set-address' onChange={(e) => setAddress(e.target.value)}/>
         </label>
         <label>Category:
-        <input list='category' data-cy='set-category' placeholder='Choose Category' onChange={(e) => setCategory(e.target.value)}/>
+        <input list='category' className='category-input' data-cy='set-category' placeholder='Choose Category' onChange={(e) => setCategory(e.target.value)}/>
           <datalist id='category'>
             <option value='Animal Care'></option>
             <option value='Campaigning'></option>
@@ -72,10 +72,10 @@ const SubmitButton = () => {
           </datalist>
           </label>
         <label>Volunteers Needed:
-          <input type='number' min={1} max={100} data-cy='set-volunteer-num' onChange={(e) => setVolunteers((parseInt(e.target.value)))}/>
+          <input type='number' className='vol-num-input' min={1} max={100} data-cy='set-volunteer-num' onChange={(e) => setVolunteers((parseInt(e.target.value)))}/>
         </label>
         <label>Event Description:
-          <textarea placeholder='Description...' data-cy='set-description' onChange={(e) => setDescription(e.target.value)}></textarea>
+          <textarea placeholder='Description...' className='description-input' data-cy='set-description' onChange={(e) => setDescription(e.target.value)}></textarea>
         </label>
         <SubmitButton />
       </form>
