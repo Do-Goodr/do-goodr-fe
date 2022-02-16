85b96e68457326346c014598f0c41938bc529b20
Michele Comfort <michelecomfort@gmail.com>
2022-02-15 17:27:02 -0800

    Adjust helper functions to be TS files

--- src/utilities/mileageCheck.ts
+++ src/utilities/mileageCheck.ts
@@ -1,6 +1,6 @@
 import React from "react"
 
-const mileageCheck = (mileage) => {
+const mileageCheck = (mileage:number) => {
     if (mileage) {
         if ( mileage === 5 || mileage === 10 || mileage === 20 || mileage === 50) {
             return true
